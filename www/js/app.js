85ce519d0144383b5aab8a93d1eeaf4bce9c6300
Leo <fallangelli@hotmail.com>
2015-10-27 16:33:12 +0800

    finish atr

--- www/js/app.js
+++ www/js/app.js
@@ -1,14 +1,14 @@
 angular.module('zeus', ['ionic', 'zeus.controllers', 'zeus.services'])
   .constant('ApiEndpoint', {
-    his_url: 'http://localhost:8100/his_data/',
-    hq_url: 'http://localhost:8100/real_hq/',
-    gf_url: 'http://localhost:8100/gf/',
-    img_url: 'http://localhost:8100/img_sina/'
+    //his_url: 'http://localhost:8100/his_data/',
+    //hq_url: 'http://localhost:8100/real_hq/',
+    //gf_url: 'http://localhost:8100/gf/',
+    //img_url: 'http://localhost:8100/img_sina/'
 
-    //his_url: 'http://vip.stock.finance.sina.com.cn/corp/go.php/vMS_MarketHistory/stockid/',
-    //hq_url: 'http://hq.sinajs.cn/',
-    //gf_url: 'http://www.gf.com.cn/match/nxsy/',
-    //img_url: 'http://image.sinajs.cn/newchart/'
+    his_url: 'http://vip.stock.finance.sina.com.cn/corp/go.php/vMS_MarketHistory/stockid/',
+    hq_url: 'http://hq.sinajs.cn/',
+    gf_url: 'http://www.gf.com.cn/match/nxsy/',
+    img_url: 'http://image.sinajs.cn/newchart/'
   })
 
   .run(function ($ionicPlatform) {
