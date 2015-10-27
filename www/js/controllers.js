ec2cfb6043b2efe86248e3a2a68773c0e13ebd6d
Leo <fallangelli@hotmail.com>
2015-10-27 09:30:52 +0800

    finish atr

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,28 +1,460 @@
-angular.module('starter.controllers', [])
+angular.module('zeus.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
 
-.controller('ChatsCtrl', function($scope, Chats) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+  .controller('TodoController', function ($scope, $q, $timeout, $ionicActionSheet, $ionicModal, Positions, HisData, $ionicSideMenuDelegate, $http) {
 
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  };
-})
+    $scope.updateHisData = function (position) {
+      var numCode = position.code.replace(/[a-zA-Z]+/, '');
 
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
-})
+      var now = new Date();
+      var quarter = getQuarterByMonth(now.getMonth());
+      var year = now.getFullYear();
 
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
-});
+      var hisData = new Array();
+      HisData.query(numCode, year, quarter).then(function (res) {
+          for (item in res) {
+            if (hisData.length >= 16)
+              break;
+            hisData[item] = res[item];
+          }
+
+          if (quarter > 1) {
+            quarter = quarter - 1;
+          }
+          else {
+            quarter = 4;
+            year = year - 1;
+          }
+
+          HisData.query(numCode, year, quarter).then(function (res) {
+            for (item in res) {
+              if (hisData.length >= 16)
+                break;
+              hisData[hisData.length] = res[item];
+            }
+            position.hisData = hisData;
+            Positions.saveOne(position);
+
+            if (quarter > 1) {
+              quarter = quarter - 1;
+            }
+            else {
+              quarter = 4;
+              year = year - 1;
+            }
+            HisData.query(numCode, year, quarter).then(function (res) {
+              for (item in res) {
+                if (hisData.length >= 16)
+                  break;
+                hisData[hisData.length] = res[item];
+              }
+              position.hisData = hisData;
+              Positions.saveOne(position);
+            });
+          });
+        }
+      )
+      ;
+    }
+
+
+//300涨
+    $scope.open_878002 = function (code) {
+      window.open("/gf/match/index_dkgg.jsp?code=878002", "_blank", "location=no,toolbar=no");
+    };
+
+//300跌
+    $scope.open_878003 = function (code) {
+      window.open("/gf/match/index_dkgg.jsp?code=878003", "_blank", "location=no,toolbar=no");
+    };
+
+
+    $scope.open300 = function (code) {
+      window.open("/img/sina/min/n/sh000300.gif", "_blank", "location=no,toolbar=no");
+    };
+
+    $scope.open300k = function (code) {
+      window.open("/img/sina/daily/n/sh000300.gif", "_blank", "location=no,toolbar=no");
+    };
+
+
+//创业涨
+    $scope.open_878004 = function (code) {
+      window.open("/gf/match/index_dkgg.jsp?code=878004", "_blank", "location=no,toolbar=no");
+    };
+
+//创业跌
+    $scope.open_878005 = function (code) {
+      window.open("/gf/match/index_dkgg.jsp?code=878005", "_blank", "location=no,toolbar=no");
+    };
+
+
+    $scope.openCY = function (code) {
+      window.open("/img/sina/min/n/sz399006.gif", "_blank", "location=no,toolbar=no");
+    };
+
+    $scope.openCYk = function (code) {
+      window.open("/img/sina/daily/n/sz399006.gif", "_blank", "location=no,toolbar=no");
+    };
+
+//50涨
+    $scope.open_878006 = function (code) {
+      window.open("/gf/match/index_dkgg.jsp?code=878006", "_blank", "location=no,toolbar=no");
+    };
+
+//50跌
+    $scope.open_878007 = function (code) {
+      window.open("/gf/match/index_dkgg.jsp?code=878007", "_blank", "location=no,toolbar=no");
+    };
+
+    $scope.open50 = function (code) {
+      window.open("/img/sina/min/n/sh000016.gif", "_blank", "location=no,toolbar=no");
+    };
+
+    $scope.open50k = function (code) {
+      window.open("/img/sina/daily/n/sh000016.gif", "_blank", "location=no,toolbar=no");
+    };
+
+
+//500涨
+    $scope.open_878008 = function (code) {
+      window.open("/gf/match/index_dkgg.jsp?code=878008", "_blank", "location=no,toolbar=no");
+    };
+
+//500跌
+    $scope.open_878009 = function (code) {
+      window.open("/gf/match/index_dkgg.jsp?code=878009", "_blank", "location=no,toolbar=no");
+    };
+
+    $scope.open500 = function (code) {
+      window.open("/img/sina/min/n/sh000905.gif", "_blank", "location=no,toolbar=no");
+    };
+
+    $scope.open500k = function (code) {
+      window.open("/img/sina/daily/n/sh000905.gif", "_blank", "location=no,toolbar=no");
+    };
+
+    $scope.openDayGif = function (code) {
+      window.open("/img/sina/daily/n/" + code + ".gif", "_blank", "location=no,toolbar=no");
+    };
+
+    $scope.openMinGif = function (code) {
+      window.open("/img/sina/min/n/" + code + ".gif", "_blank", "location=no,toolbar=no");
+    };
+
+
+    $scope.refreshAll = function () {
+      $timeout(function () {
+        var positions = Positions.all();
+        for (var i = 0; i < positions.length; i++) {
+          loadRunTimeData(positions[i]);
+          $scope.updateHisData(positions[i]);
+          $scope.fillPosition(positions[i]);
+        }
+
+        $scope.positions = positions;
+
+        $scope.$broadcast('scroll.refreshComplete');
+      }, 500);
+
+    };
+
+    $scope.doRefresh = function () {
+      $timeout(function () {
+        //simulate async response
+        Positions.setLastActiveIndex(Positions.getLastActiveIndex());
+        var pos = $scope.positions[Positions.getLastActiveIndex()];
+
+        loadRunTimeData(pos);
+        $scope.updateHisData(pos);
+        $scope.fillPosition(pos);
+
+        $scope.activePosition = pos;
+
+        $scope.$broadcast('scroll.refreshComplete');
+      }, 500);
+
+    };
+
+    var getQuarterByMonth = function (month) {
+      if (month <= 2) {
+        return 1;
+      }
+      else if (month <= 5) {
+        return 2;
+      }
+      else if (month <= 8) {
+        return 3;
+      }
+      else {
+        return 4;
+      }
+    }
+
+
+    var loadRunTimeData = function (position) {
+      //var data = 'hq_str_sz000913="钱江摩托,8.56,8.32,14.30,9.15,8.48,8.75,8.76,31652880,278885232.67,49100,8.75,10700,8.74,73000,8.73,112500,8.72,4300,8.71,12100,8.76,29500,8.77,28000,8.78,43900,8.79,195100,8.80,2015-10-13,11:35:52,00"';
+      var myUrl = "/remote/hq/list=" + position.code;
+      $http.get(myUrl).success(function (data, status, headers, config) {
+          var temp = data.split(',')[0];
+          position.title = temp.substring(temp.indexOf('"') + 1, temp.length);
+
+          position.todayStart = data.split(',')[1];
+          position.yestodayEnd = data.split(',')[2];
+          position.currPrice = data.split(',')[3];
+          position.currMount = position.currPrice * position.initialCount;
+          position.currMount = position.currMount.toFixed(2);
+
+
+          position.currDayPriceColor = {color: 'blue'};
+          if (position.currPrice && position.yestodayEnd) {
+            if (parseFloat(position.currPrice) < parseFloat(position.yestodayEnd))
+              position.currDayPriceColor = {color: 'darkgreen'};
+            else if (parseFloat(position.yestodayEnd) <= parseFloat(position.currPrice))
+              position.currDayPriceColor = {color: 'darkRed'};
+
+            position.currDayChangePercent = (position.currPrice - position.yestodayEnd) * 100 / position.yestodayEnd;
+            position.currDayChange = (position.currPrice - position.yestodayEnd) * position.initialCount;
+
+            position.currDayChangePercent = position.currDayChangePercent.toFixed(3);
+            position.currDayChange = position.currDayChange.toFixed(2);
+          }
+
+
+          position.currPriceColor = {color: 'blue'};
+          if (position.currPrice) {
+            if (parseFloat(position.currPrice) < parseFloat(position.lowStopPrice))
+              position.currPriceColor = {color: 'green'};
+            else if (parseFloat(position.lowStopPrice) <= parseFloat(position.currPrice) && parseFloat(position.currPrice) < parseFloat(position.initialPrice))
+              position.currPriceColor = {color: 'darkgreen'};
+            else if (parseFloat(position.initialPrice) <= parseFloat(position.currPrice) && parseFloat(position.currPrice) < parseFloat(position.highStopPrice1))
+              position.currPriceColor = {color: 'orange'};
+            else if (parseFloat(position.highStopPrice1) <= parseFloat(position.currPrice) && parseFloat(position.currPrice) < parseFloat(position.highStopPrice2))
+              position.currPriceColor = {color: 'palevioletred'};
+            else if (parseFloat(position.highStopPrice2) <= parseFloat(position.currPrice) && parseFloat(position.currPrice) < parseFloat(position.highStopPrice3))
+              position.currPriceColor = {color: 'darkred'};
+            else if (parseFloat(position.highStopPrice3) <= parseFloat(position.currPrice))
+              position.currPriceColor = {color: 'red'};
+          }
+
+          position.realhighStopPrice = '无';
+          position.currHighPriceColor = {color: 'blue'};
+          if (position.currPrice) {
+            var atr = position.currATR ? position.currATR : position.initialATR;
+
+            if (parseFloat(position.highStopPrice1) <= parseFloat(position.currPrice) && parseFloat(position.currPrice) < parseFloat(position.highStopPrice2)) {
+              position.realhighStopPrice = position.currPrice - 4 * atr;
+              position.realhighStopPrice = position.realhighStopPrice.toFixed(2);
+              position.currHighPriceColor = position.currPriceColor;
+            }
+            else if (parseFloat(position.highStopPrice2) <= parseFloat(position.currPrice) && parseFloat(position.currPrice) < parseFloat(position.highStopPrice3)) {
+              position.realhighStopPrice = position.currPrice - 3 * atr;
+              position.realhighStopPrice = position.realhighStopPrice.toFixed(2);
+              position.currHighPriceColor = position.currPriceColor;
+            }
+            else if (parseFloat(position.highStopPrice3) <= parseFloat(position.currPrice)) {
+              position.realhighStopPrice = position.currPrice - 2 * atr;
+              position.realhighStopPrice = position.realhighStopPrice.toFixed(2);
+              position.currHighPriceColor = position.currPriceColor;
+            }
+          }
+
+          if (position.currPrice && position.initialPrice && position.initialCount) {
+            position.currChangePercent = (position.currPrice - position.initialPrice) * 100 / position.initialPrice;
+            position.currChange = (position.currPrice - position.initialPrice) * position.initialCount;
+
+            position.currChangePercent = position.currChangePercent.toFixed(3);
+            position.currChange = position.currChange.toFixed(2);
+          }
+
+        }
+      ).
+        error(function (data, status, headers, config) {
+          alert("读取实时信息错误");
+        }
+      );
+
+
+    }
+// A utility function for creating a new position
+// with the given positionCode
+    var createPosition = function (positionCode) {
+
+      var newPosition = Positions.newPosition(positionCode);
+      if (positionCode.substr(0, 1) == '6')
+        newPosition.code = 'sh' + positionCode;
+      if (positionCode.substr(0, 1) == '0' || positionCode.substr(0, 1) == '3')
+        newPosition.code = 'sz' + positionCode;
+
+      $scope.updateHisData(newPosition);
+
+      $scope.positions.push(newPosition);
+      Positions.save($scope.positions);
+      $scope.selectPosition(newPosition, $scope.positions.length - 1);
+    }
+
+
+// Load or initialize positions
+    $scope.positions = Positions.all();
+
+
+// Grab the last active, or the first position
+    var currPos = $scope.positions[Positions.getLastActiveIndex()];
+
+
+    $scope.activePosition = currPos;
+
+// Called to create a new position
+    $scope.newPosition = function () {
+      var positionCode = prompt('代码');
+      if (positionCode) {
+        createPosition(positionCode);
+      }
+    };
+
+// Called to select the given position
+    $scope.selectPosition = function (position, index) {
+
+      Positions.setLastActiveIndex(index);
+      var pos = $scope.positions[Positions.getLastActiveIndex()];
+
+      loadRunTimeData(pos);
+      $scope.updateHisData(pos);
+      $scope.fillPosition(pos);
+      $scope.activePosition = pos;
+      $ionicSideMenuDelegate.toggleLeft(false);
+    };
+
+
+    $scope.delPosition = function (position, index) {
+      Positions.delPosition(index);
+      $scope.positions = Positions.all();
+      Positions.setLastActiveIndex(0);
+      $scope.activePosition = $scope.positions[Positions.getLastActiveIndex()];
+      $ionicSideMenuDelegate.toggleLeft(false);
+    };
+
+
+    $scope.fillPosition = function (position) {
+      if (position) {
+
+        //建议仓位数量
+        position.advicePosition = position.totalFund * position.positionRC / ( position.initialATR * position.stopAM * 100);
+        //建议仓位金额
+        if (position.initialPrice)
+          position.advicePosFund = position.advicePosition * position.initialPrice;
+        else
+          position.advicePosFund = position.advicePosition * position.currPrice;
+        //止损价
+        position.stopPercent = position.stopAM * position.initialATR / position.initialPrice;
+
+        position.stopFund = position.stopPercent * position.initialCount * position.initialPrice;
+
+        position.lowStopPrice = position.initialPrice * (1 - position.stopPercent);
+
+        position.highStopPrice1 = position.initialPrice * (1 + position.stopPercent * 2);
+
+        position.highStopPrice2 = position.initialPrice * (1 + position.stopPercent * 4);
+
+        position.highStopPrice3 = position.initialPrice * (1 + position.stopPercent * 6);
+
+        //计算tr
+        if (position.hisData && position.hisData.length > 0) {
+          var index = 0;
+          while (index <= 15) {
+            var itemBaseData = position.hisData[index];
+            var itemLastData = position.hisData[index + 1];
+
+            if (typeof(itemLastData) == "undefined")
+              break;
+
+            var tr = Math.max(Math.abs(itemBaseData.maxP - itemBaseData.minP), Math.abs(itemLastData.closingP - itemBaseData.maxP),
+              Math.abs(itemLastData.closingP - itemBaseData.minP));
+
+            itemBaseData['tr'] = tr;
+            index++;
+          }
+          var sumTR = 0;
+          for (var i = 14; i > 0; i--) {
+            sumTR += position.hisData[i].tr;
+          }
+          position.currATR = parseFloat(sumTR) / 13 + parseFloat(position.hisData[0].tr) / 14;
+          position.currATR = position.currATR.toFixed(2);
+        }
+
+
+        position.advicePosition = position.advicePosition.toFixed(0);
+        position.advicePosFund = position.advicePosFund.toFixed(2);
+        position.lowStopPrice = position.lowStopPrice.toFixed(2);
+        position.stopFund = position.stopFund.toFixed(2);
+        position.stopPercent = position.stopPercent.toFixed(3);
+        position.highStopPrice1 = position.highStopPrice1.toFixed(2);
+        position.highStopPrice2 = position.highStopPrice2.toFixed(2);
+        position.highStopPrice3 = position.highStopPrice3.toFixed(2);
+
+        Positions.saveOne(position);
+      }
+    }
+// Create our modal
+    $ionicModal.fromTemplateUrl('new-task.html', function (modal) {
+      $scope.taskModal = modal;
+    }, {
+      scope: $scope
+    });
+
+    $scope.createTask = function (position) {
+      if (!$scope.activePosition || !position) {
+        return;
+      }
+
+      $scope.activePosition.totalFund = position.totalFund;
+      $scope.activePosition.initialCount = position.initialCount;
+      $scope.activePosition.initialPrice = position.initialPrice;
+      $scope.activePosition.initialATR = position.currATR;
+      $scope.activePosition.positionRC = position.positionRC;
+      $scope.activePosition.stopAM = position.stopAM;
+
+      loadRunTimeData($scope.activePosition);
+      $scope.updateHisData($scope.activePosition);
+      $scope.fillPosition($scope.activePosition);
+
+      $scope.taskModal.hide();
+
+      // Inefficient, but save all the positions
+      Positions.save($scope.positions);
+
+    };
+
+    $scope.newTask = function () {
+      $scope.taskModal.show();
+    };
+
+    $scope.closeNewTask = function () {
+      $scope.taskModal.hide();
+    }
+
+    $scope.toggleLeft = function () {
+      $ionicSideMenuDelegate.toggleLeft();
+    };
+
+
+    $scope.toggleRight = function () {
+      $ionicSideMenuDelegate.toggleRight();
+    };
+// Try to create the first position, make sure to defer
+// this by using $timeout so everything is initialized
+// properly
+    $timeout(function () {
+      if ($scope.positions.length == 0) {
+        while (true) {
+          var positionCode = prompt('名称:');
+          if (positionCode) {
+            createPosition(positionCode);
+            break;
+          }
+        }
+      }
+    });
+
+  }
+);
--- www/js/services.js
+++ www/js/services.js
@@ -1,50 +1,186 @@
-angular.module('starter.services', [])
+angular.module('zeus.services', [])
 
-.factory('Chats', function() {
-  // Might use a resource here that returns a JSON array
+  .factory('Positions', function () {
+    return {
+      all: function () {
+        var positionString = window.localStorage['positions'];
+        if (positionString) {
+          var pos = angular.fromJson(positionString);
 
-  // Some fake testing data
-  var chats = [{
-    id: 0,
-    name: 'Ben Sparrow',
-    lastText: 'You on your way?',
-    face: 'https://pbs.twimg.com/profile_images/514549811765211136/9SgAuHeY.png'
-  }, {
-    id: 1,
-    name: 'Max Lynx',
-    lastText: 'Hey, it\'s me',
-    face: 'https://avatars3.githubusercontent.com/u/11214?v=3&s=460'
-  }, {
-    id: 2,
-    name: 'Adam Bradleyson',
-    lastText: 'I should buy a boat',
-    face: 'https://pbs.twimg.com/profile_images/479090794058379264/84TKj_qa.jpeg'
-  }, {
-    id: 3,
-    name: 'Perry Governor',
-    lastText: 'Look at my mukluks!',
-    face: 'https://pbs.twimg.com/profile_images/598205061232103424/3j5HUXMY.png'
-  }, {
-    id: 4,
-    name: 'Mike Harrington',
-    lastText: 'This is wicked good ice cream.',
-    face: 'https://pbs.twimg.com/profile_images/578237281384841216/R3ae1n61.png'
-  }];
+          return pos;
+        }
+        return [];
+      },
+      get: function (code) {
+        var positionString = window.localStorage['positions'];
+        if (positionString) {
+          var pos = angular.fromJson(positionString);
+          for (var i = 0; i < pos.length; i++) {
+            if (pos[i].code == code) {
+              return pos[i];
+            }
+          }
+          return null;
+        }
+        return [];
+      },
+      saveOne: function (position) {
+        var allPos = this.all();
+        for (var i = 0; i < allPos.length; i++) {
+          if (allPos[i].code == position.code) {
+            allPos[i] = position;
+            this.save(allPos);
+          }
+        }
+      },
+      save: function (positions) {
+        window.localStorage['positions'] = angular.toJson(positions);
+      }
+      ,
+      newPosition: function (positionCode) {
+        // Add a new position
+        return {
+          code: positionCode
+        };
+      }
+      ,
+      getLastActiveIndex: function () {
+        return parseFloat(window.localStorage['lastActivePosition']) || 0;
+      }
+      ,
+      setLastActiveIndex: function (index) {
+        window.localStorage['lastActivePosition'] = index;
+      }
+      ,
+      delPosition: function (index) {
 
-  return {
-    all: function() {
-      return chats;
-    },
-    remove: function(chat) {
-      chats.splice(chats.indexOf(chat), 1);
-    },
-    get: function(chatId) {
-      for (var i = 0; i < chats.length; i++) {
-        if (chats[i].id === parseInt(chatId)) {
-          return chats[i];
+        var positionString = window.localStorage['positions'];
+        if (positionString) {
+          var pos = angular.fromJson(positionString);
+
+
+          if (isNaN(index) || index > pos.length) {
+            return false;
+          }
+          for (var i = 0, n = 0; i < pos.length; i++) {
+            if (pos[i] != pos[index]) {
+              pos[n++] = pos[i];
+            }
+          }
+          pos.length -= 1;
+          window.localStorage['positions'] = angular.toJson(pos);
         }
       }
-      return null;
     }
-  };
-});
+  })
+
+
+  .factory('HisData', ['$http', '$q', function ($http, $q) {
+    return {
+
+      query: function (numCode, year, quarter) {
+        var deferred = $q.defer(); // 声明延后执行，表示要去监控后面的执行
+        var myUrl = "/remote/his/" + numCode + ".phtml?year=" + year + "&jidu=" + quarter;
+        $http({method: 'GET', url: myUrl}).
+          success(function (data, status, headers, config) {
+            var strExp = "<a target='_blank'\\s+href='http://vip.stock.finance.sina.com.cn/quotes_service/view/vMS_tradehistory.php\\?";
+            strExp += "symbol=\\w{8}&date=\\d{4}-\\d{2}-\\d{2}'>\\s*([^\\s]+)\\s+</a>\\s*</div></td>";
+            strExp += "\\s*<td[^\\d]*([^<]*)</div></td>\\s+<td[^\\d]*([^<]*)</div></td>\\s+<td[^\\d]*([^<]*)</div></td>\\s+<td[^\\d]*([^<]*)</div></td>\\s";
+            var regexp = new RegExp(strExp, "g");
+            var temp = data.match(regexp);
+            var hisDataIndex = 0;
+            var hisData = new Object();
+            for (var item in temp) {
+              var parseData = parseHis(temp[item]);
+              hisData[hisDataIndex] = parseData;
+              hisDataIndex++;
+            }
+            deferred.resolve(hisData);  // 声明执行成功，即http请求数据成功，可以返回数据了
+          }).
+          error(function (data, status, headers, config) {
+            deferred.reject(data);   // 声明执行失败，即服务器返回错误
+          });
+        return deferred.promise;   // 返回承诺，这里并不是最终数据，而是访问最终数据的API
+      } // end query
+    };
+  }])
+
+  .factory('Chats', function () {
+    // Might use a resource here that returns a JSON array
+
+    // Some fake testing data
+    var chats = [{
+      id: 0,
+      name: 'Ben Sparrow',
+      lastText: 'You on your way?',
+      face: 'https://pbs.twimg.com/profile_images/514549811765211136/9SgAuHeY.png'
+    }, {
+      id: 1,
+      name: 'Max Lynx',
+      lastText: 'Hey, it\'s me',
+      face: 'https://avatars3.githubusercontent.com/u/11214?v=3&s=460'
+    }, {
+      id: 2,
+      name: 'Adam Bradleyson',
+      lastText: 'I should buy a boat',
+      face: 'https://pbs.twimg.com/profile_images/479090794058379264/84TKj_qa.jpeg'
+    }, {
+      id: 3,
+      name: 'Perry Governor',
+      lastText: 'Look at my mukluks!',
+      face: 'https://pbs.twimg.com/profile_images/598205061232103424/3j5HUXMY.png'
+    }, {
+      id: 4,
+      name: 'Mike Harrington',
+      lastText: 'This is wicked good ice cream.',
+      face: 'https://pbs.twimg.com/profile_images/578237281384841216/R3ae1n61.png'
+    }];
+
+    return {
+      all: function () {
+        return chats;
+      },
+      remove: function (chat) {
+        chats.splice(chats.indexOf(chat), 1);
+      },
+      get: function (chatId) {
+        for (var i = 0; i < chats.length; i++) {
+          if (chats[i].id === parseInt(chatId)) {
+            return chats[i];
+          }
+        }
+        return null;
+      }
+    };
+  });
+
+
+function parseHis(content) {
+  var result = new Object();
+  var tmpStr = new String(content);
+  var tmpStart = tmpStr.indexOf("&date=");
+  tmpStr = tmpStr.substr(tmpStart + 6);
+  result.dateP = tmpStr.substring(0, tmpStr.indexOf("'>"));
+
+  tmpStr = tmpStr.substr(tmpStr.indexOf("'>") + 2);
+  tmpStart = tmpStr.indexOf('align="center">');
+  tmpStr = tmpStr.substr(tmpStart + 15);
+  result.openingP = tmpStr.substring(0, tmpStr.indexOf("</"));
+
+  tmpStr = tmpStr.substr(tmpStr.indexOf("'>") + 2);
+  tmpStart = tmpStr.indexOf('align="center">');
+  tmpStr = tmpStr.substr(tmpStart + 15);
+  result.maxP = tmpStr.substring(0, tmpStr.indexOf("</"));
+
+  tmpStr = tmpStr.substr(tmpStr.indexOf("'>") + 2);
+  tmpStart = tmpStr.indexOf('align="center">');
+  tmpStr = tmpStr.substr(tmpStart + 15);
+  result.closingP = tmpStr.substring(0, tmpStr.indexOf("</"));
+
+  tmpStr = tmpStr.substr(tmpStr.indexOf("'>") + 2);
+  tmpStart = tmpStr.indexOf('align="center">');
+  tmpStr = tmpStr.substr(tmpStart + 15);
+  result.minP = tmpStr.substring(0, tmpStr.indexOf("</"));
+
+  return result;
+};
